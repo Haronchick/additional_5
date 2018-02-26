3a3b022b51d1b857386a6aed8325cafd90bd1b88
haronchick <haron.www@mail.ru>
2018-02-26 20:46:48 +0300

    edit solution task additional_5

--- src/index.js
+++ src/index.js
@@ -1,27 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-  let  openBracket = str[0];
+    // your solution
+    let bracketOpen = 0, bracketClose = 0, result = true, arrBrackets = [];
+    if (str.length%2 === 1) return false;
+    for (let i = 0; i < str.length && result === true; i++) {
+        //  console.log(str[i]);
+        //  console.log(bracketsConfig);
+        let config = findConfig(str[i], bracketsConfig);
 
-  let config = findConfig(openBracket,bracketsConfig);
+        //console.log(config);
+        if (str[i] === config[0]) arrBrackets.push(str[i]);  //if (str[i] === config[0]) bracketOpen++;
+        if (str[i] === config[1]) {
+            console.log(arrBrackets[arrBrackets.length - 1]);
+            console.log(config[0]);
+            if(arrBrackets[arrBrackets.length - 1] !== config[0]) {
+                result = false;
+            }else arrBrackets.pop();
+        }
+        //if (str[i] === config[1]) bracketClose++;
+       // if (bracketOpen < bracketClose) result = false;
+        // console.log(bracketOpen);
+        // console.log(bracketClose);
+    }
 
+  //  if (bracketOpen !== bracketClose) result = false;
+    if (arrBrackets === []) result = true;
 
-  let arrStr = str.split('');
-  console.log(arrStr);
-  let index = arrStr.findIndex((elem) => {
-      return elem === config[1];
-  });
- // console.log(index);
-
-
-
-    if (config === undefined || index%2 === 0 ) return false;
+    // let config = findConfig(openBracket,bracketsConfig);
 
     function findConfig(symbol, configs) {
         return configs.find((elem) => {
-            return elem[0] === symbol;
+            //  console.log('elem');
+            //  console.log(elem);
+            return elem[0] === symbol || elem[1] === symbol;
         });
     }
-    str = str.substr(1);
- return str.length > 1 ? check(str, bracketsConfig) : true;
+
+    return result;
 
 }
