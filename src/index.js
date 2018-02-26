913d8f55406eca9f8d7a3580367449a6b4fddd4c
haronchick <haron.www@mail.ru>
2018-02-26 18:13:45 +0300

    solution task additional_5, test - 6 ok, 14 fail | add file .gitignore

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let  openBracket = str[0];
+
+  let config = findConfig(openBracket,bracketsConfig);
+
+
+  let arrStr = str.split('');
+  console.log(arrStr);
+  let index = arrStr.findIndex((elem) => {
+      return elem === config[1];
+  });
+ // console.log(index);
+
+
+
+    if (config === undefined || index%2 === 0 ) return false;
+
+    function findConfig(symbol, configs) {
+        return configs.find((elem) => {
+            return elem[0] === symbol;
+        });
+    }
+    str = str.substr(1);
+ return str.length > 1 ? check(str, bracketsConfig) : true;
+
 }
